351935dc0f699b63b1d8f03d200983fdca41088f
ilaria0401 <ilariamas.94@gmail.com>
2023-12-01 17:43:31 +0100

    setup progetto servizi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,OnInit, ViewChild, } from '@angular/core';
+import { FormGroup,FormControl,FormBuilder,FormsModule } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AppService } from './app.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[AppService,HttpClient]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TEST-SERVIZIO';
+  @ViewChild('modalInserimento')modalInserimento!:ElementRef;
+  FormSearch:FormGroup;
+
+
+  arreyListaServer=[
+    {
+      fattureIpaId:0,
+      codice:'',
+      denominazione:''
+    }
+  ];
+
+  serverFilter=[...this.arreyListaServer]
+
+  constructor(private fb: FormBuilder, private modalService: NgbModal, private restService: AppService) {
+    this.FormSearch=this.fb.group({
+     fattureIpaId : new FormControl (''),
+     codice : new FormControl (''),
+     denominazione : new FormControl (''),
+
+    })
+
+    console.log("arreyListaServer >>", this.arreyListaServer);
+
+    this.restService.listIpa().then(
+      res => {
+        let arrayFiltrato = [...res.ipaBindList].filter(ipa => ipa.fattureIpaId>0);
+
+        this.arreyListaServer = arrayFiltrato
+        console.log("arreyListaServer 2>>", this.arreyListaServer);
+
+
+      }
+    )
+
+  }
+
+  ngOnInit(): void {}
+
+  apriModalInserimento(){
+    this.modalService.open(this.modalInserimento, {size: 'lg' })
+  }
+
 }
+
+
+
+
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  listIpa(): Promise<any>{
+    return this.http.get("https://unisa-sviluppo.ddns.net:8443/UNISAGatewayRest/rest/fatture/listIpa").toPromise().then(
+      res => res).catch();
+
+  
+}
 }
